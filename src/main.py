1dffee1ed2111533e3ab125096e296ce3a4b22f9
miromatagne <miromatagne@users.noreply.github.com>
2020-12-06 17:27:51 +0100

    started trying out kivy

--- src/main.py
+++ src/main.py
@@ -1,1 +1,29 @@
-# Handles Kivy UI and calls other files
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+
+
+class MainWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("ui.kv")
+
+
+class Program(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    Program().run()
