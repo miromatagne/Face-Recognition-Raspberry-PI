36b6d88975e43ba9f4dea1a26dd4187c72c80d18
miromatagne <miromatagne@users.noreply.github.com>
2021-02-04 00:24:32 +0100

    database

--- src/database.py
+++ src/database.py
@@ -4,27 +4,29 @@ from bson.objectid import ObjectId
 # TODO: change URL and cluster name
 cluster = MongoClient(
     "mongodb+srv://miromatagne:Mmm2000(@proj-h-402.zpee0.mongodb.net/<dbname>?retryWrites=true&w=majority")
-db = cluster["proj-h-402"]
+db = cluster["Test"]
 collection = db["Users"]
 
 
 def post_to_db():
-    post = {"name": "Miro", "age": 20,
-            "photo": "https://storage.cloud.google.com/verylonguniquebucketname/Images/Miro1.JPG"}
+    post = {"name": "Miro", "age": 20, "encodings": [[1, 2, 3], [4, 5, 6]]}
 
     collection.insert_one(post)
 
 
-def add_image():
-    collection.update_one({"_id": ObjectId("5fc61ba448f61a6793b485cf")}, {'$push': {
-        'photos': "https://media.resources.festicket.com/www/artists/KendrickLamar_New.jpg"}})
+# def add_image():
+#     collection.update_one({"_id": ObjectId("5fc61ba448f61a6793b485cf")}, {'$push': {
+#         'photos': "https://media.resources.festicket.com/www/artists/KendrickLamar_New.jpg"}})
 
 
 def get_documents():
     documents = collection.find({})
-    user_pictures = {}
+    users = {}
     for doc in documents:
-        user_pictures[doc["_id"]]
+        user = {"name": doc["name"], "age": doc["age"]}
+        encoding = doc["encoding"]
+        users[str(doc["_id"])] = {"user": user, "encoding": encoding}
+    return users
 
 
-add_image()
+post_to_db()
